9fa344bae93bf8272aec0a4e00e435efe442016d
Nguyen Hoai <50800569+nguyenhoai0910@users.noreply.github.com>
2022-06-28 22:45:17 +0700

    Update nodejs_start-app.bat

--- nodejs_start-app.bat
+++ nodejs_start-app.bat
@@ -1,2 +1,8 @@
 @echo off
 Rem des: create task scheduler run script auto start service app.js
+set date="%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%"
+C:
+if not exist "C:\logs\%date%" mkdir "C:\logs\%date%"
+cd C:\App
+pm2 start app.js
+pm2 status 0 > C:\logs\%date%
